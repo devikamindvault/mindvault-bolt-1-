fccc4b23f7a55f16545868d5356317f6896ea03c
devikamindvault <orjayasudha7@gmail.com>
2025-07-05 12:48:58 +0530

    Updated package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client", "src"),
       "@shared": path.resolve(__dirname, "shared"),
     },
   },
