fc2c5a718c8d62c6a07cc466115df773114bf4bf
devikamindvault <orjayasudha7@gmail.com>
2025-07-09 12:44:01 +0530

    Fix Image Processing in RichTextEditor

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,10 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { ThemeProvider } from "@/components/theme-provider";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-// Enable source maps in development
-if (process.env.NODE_ENV === 'development') {
-  Error.stackTraceLimit = Infinity;
-}
-
-createRoot(document.getElementById("root")!).render(
-  <ThemeProvider defaultTheme="dark">
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </ThemeProvider>
-);
+  </React.StrictMode>,
+)
--- server/db.ts
+++
@@ -1,3 +0,0 @@
-// Database removed for static app
-export const db = null;
-export const pool = null;
